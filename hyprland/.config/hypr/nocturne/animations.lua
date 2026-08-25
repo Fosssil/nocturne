c1992e401135daf192604afd0e0cbfd8f590c117
Fosssil <dahiyabadshah.fossil@gmail.com>
2026-08-26 02:06:51 +0530

    Hyprland: Added some basic animations

--- hyprland/.config/hypr/nocturne/animations.lua
+++ hyprland/.config/hypr/nocturne/animations.lua
@@ -24,6 +24,114 @@ hl.curve("nocturneSmooth", {
 hl.curve("nocturneSpring", {
 	type = "spring",
 	mass = 1,
-	stiffness = 70,
-	dampening = 12,
+	stiffness = 78,
+	dampening = 9.5,
+})
+
+hl.curve("nocturneMotion", {
+	type = "spring",
+	mass = 1,
+	stiffness = 105,
+	dampening = 14,
+})
+
+hl.curve("nocturneWorkspace", {
+	type = "bezier",
+	points = {
+		{ 0.45, 0.0 },
+		{ 0.15, 1.0 },
+	},
+})
+
+-- ─[ Global ]────────────────────────────────────────────────────────
+
+hl.config({
+	animations = {
+		enabled = true,
+		workspace_wraparound = true,
+	},
+})
+
+hl.animation({
+	leaf = "global",
+	enabled = true,
+	speed = 7,
+	bezier = "nocturneEase",
+})
+
+-- ─[ Windows ]───────────────────────────────────────────────────────
+
+hl.animation({
+	leaf = "windows",
+	enabled = true,
+	speed = 5,
+	spring = "nocturneSpring",
+	style = "popin",
+})
+
+hl.animation({
+	leaf = "windowsIn",
+	enabled = true,
+	speed = 5,
+	spring = "nocturneSpring",
+	style = "popin 82%",
+})
+
+hl.animation({
+	leaf = "windowsOut",
+	enabled = true,
+	speed = 7,
+	bezier = "nocturneEase",
+	style = "popin 90%",
+})
+
+hl.animation({
+	leaf = "windowsMove",
+	enabled = true,
+	speed = 4,
+	-- spring = "nocturneSpring",
+	spring = "nocturneMotion",
+})
+
+-- ─[ Workspaces ]────────────────────────────────────────────────────
+
+hl.animation({
+	leaf = "workspaces",
+	enabled = true,
+	speed = 4,
+	bezier = "nocturneWorkspace",
+	style = "slidefade 25%",
+})
+
+hl.animation({
+	leaf = "workspacesIn",
+	enabled = true,
+	speed = 4,
+	bezier = "nocturneWorkspace",
+	style = "slidefade 25%",
+})
+
+hl.animation({
+	leaf = "workspacesOut",
+	enabled = true,
+	speed = 4,
+	bezier = "nocturneWorkspace",
+	style = "slidefade 25%",
+})
+
+-- ─[ Focus ]─────────────────────────────────────────────────────────
+
+hl.animation({
+	leaf = "fadeSwitch",
+	enabled = true,
+	speed = 2,
+	bezier = "nocturneEase",
+})
+
+hl.animation({
+	leaf = "specialWorkspace",
+	enabled = true,
+	speed = 6,
+	bezier = "nocturneSmooth",
+	style = "slidefade 20%",
 })
