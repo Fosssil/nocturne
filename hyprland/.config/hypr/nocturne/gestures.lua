6f8070969fb6d8731408592395416951efc6da98
Fosssil <dahiyabadshah.fossil@gmail.com>
2026-08-25 22:57:00 +0530

    Hyprland: Added gestures

--- hyprland/.config/hypr/nocturne/gestures.lua
+++ hyprland/.config/hypr/nocturne/gestures.lua
@@ -3,3 +3,16 @@ hl.gesture({
 	direction = "horizontal",
 	action = "workspace",
 })
+
+hl.gesture({
+	fingers = 3,
+	direction = "up",
+	action = "fullscreen",
+})
+
+hl.gesture({
+	fingers = 2,
+	direction = "pinch",
+	action = "cursor_zoom",
+	zoom_level = 2,
+})
