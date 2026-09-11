9b67894e079dd49ef1c057d957584bc626044753
Fosssil <dahiyabadshah.fossil@gmail.com>
2026-09-11 23:27:47 +0530

    NVIM: Reclocated a line

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,6 +2,7 @@ local catppuccin = require("plugins.catppuccin")
 -- Faster startup
 vim.loader.enable()
 vim.o.synmaxcol = 240
+vim.o.termguicolors = true
 
 -- Set leader key
 vim.g.mapleader = " "
@@ -20,5 +21,3 @@ require("lazy").setup("plugins")
 require("core.options")
 require("core.disable")
 require("core.diagnostics")
-
-vim.o.termguicolors = true
