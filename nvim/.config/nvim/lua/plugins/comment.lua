fbcd216e689bd33030c5078c0a1dd42c5294ae0e
Fosssil <dahiyabadshah.fossil@gmail.com>
2026-09-15 23:40:42 +0530

    NVIM: Reconfigured

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -9,21 +9,17 @@ return {
 		detect_indent = true,
 		ignore_empty_lines = "always",
 		fallback_to_block = "if_line_cms_wrapped",
-
 		mappings = {
 			line_toggle = "<leader>/",
 			line_toggle_cur = "<leader>/",
 			line_toggle_visual = "<leader>/",
-
 			block_toggle = "<leader>?",
 			block_toggle_cur = "<leader>?",
 			block_toggle_visual = "<leader>?",
-
 			line_textobject = "",
 			block_textobject = "",
 			auto_textobject = "",
 			uncomment_auto = "",
-
 			line_add_below = "",
 			line_add_above = "",
 			line_add_eol = "",
